9ef2d89e3921f5a3547be9e21827502acdafbe52
Rodrigo Pinto <rodrigo.pinto@kyas.com.ar>
2024-10-20 19:36:28 -0300

    first commit

---
+++ src/app/admin/products/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminProductsPage = () => {
+  return (
+    <div>AdminProductsPage 37.46</div>
+  )
+}
+
+export default AdminProductsPage
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -12,6 +13,8 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
+const font = "--font-sans";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn("bg-background min-h-screen font-sans antialiased")}
       >
         {children}
       </body>
