9529079c05f100c0eefdf38fc276b15f204f7ffd
Rodrigo Pinto <rodrigo.pinto@kyas.com.ar>
2024-11-22 22:12:15 -0300

    Added email template

--- src/email/PurchaseReceipt.tsx
+++ src/email/PurchaseReceipt.tsx
@@ -1,32 +1,54 @@
 import React from 'react'
 import { Body, Container, Head, Heading, Html, Preview, Tailwind } from '@react-email/components';
 import OrderInformation from './components/OrderInformation';
+import { v4 as uuidv4 } from 'uuid';
 
 type PurchaseReceiptEmailProps = {
     product: {
         name: string
-    }
+    },
+    order: { 
+        id: string; 
+        createdAt: Date; 
+        pricePaidIncents: number;
+    },
+    downloadVerificationId: string
 }
 
 PurchaseReceiptEmail.PreviewProps = {
     product: {
         name: "Product name"
-    }
+    },
+    order: {
+        id: uuidv4(),
+        createdAt: new Date(),
+        pricePaidIncents: 10000
+    },
+    downloadVerificationId: uuidv4()
 } satisfies PurchaseReceiptEmailProps
 
-export function  PurchaseReceiptEmail  ({product}: PurchaseReceiptEmailProps) {
-  return (
-    <Html>
-        <Preview>Download {product.name}</Preview>
-        <Tailwind>
-            <Head />
-            <Body className='font-sans bg-white'>
-                <Container className="max-w-1">
-                    <Heading>Purchase Receipt</Heading>
-                    <OrderInformation />
-                </Container>
-            </Body>
-        </Tailwind>
-    </Html>
-  )
+export default function PurchaseReceiptEmail({ 
+    product, 
+    order, 
+    downloadVerificationId 
+}: PurchaseReceiptEmailProps) {
+    
+    return (
+        <Html>
+            <Preview>Download {product.name}</Preview>
+            <Tailwind>
+                <Head />
+                <Body className='font-sans bg-white'>
+                    <Container className="w-full">
+                        <Heading>Purchase Receipt</Heading>
+                        <OrderInformation 
+                            order={order}
+                            product={product}
+                            downloadVerificationId={downloadVerificationId}
+                        />
+                    </Container>
+                </Body>
+            </Tailwind>
+        </Html>
+    )
 }
--- src/email/components/OrderInformation.tsx
+++ src/email/components/OrderInformation.tsx
@@ -1,14 +1,43 @@
+import { formatCurrency } from '@/lib/formatters';
+import { Column, Row, Section, Text } from '@react-email/components'
 import React from 'react'
 
 type OrderInformationProps = {
-  order: {},
-  product: {},
+  order: { id: string, createdAt: Date, pricePaidIncents: number},
+  product: {name: string},
   downloadVerificationId: string
 }
 
-const OrderInformation = ({order, product, downloadVerificationId}: OrderInformationProps) => {
+const dateFormatter = new Intl.DateTimeFormat("en", { dateStyle: "medium" });
+
+export function OrderInformation ({order, product, downloadVerificationId}: OrderInformationProps) {
+  
+  if (!order) {
+    return <Text>Error: Order data is missing</Text>;
+  }
+
   return (
-    <h1>Hola este es un H1</h1>
+    <>
+      <Section className='bg-gray-100'>
+        <Row>
+          <Column>
+            <Text className='mb-0 text-gray-500 whitespace-nowrap text-nowrap'>Order ID</Text>
+            <Text className='mt-0'>{order.id}</Text>
+          </Column>
+          <Column>
+            <Text className='mb-0 text-gray-500 whitespace-nowrap text-nowrap'>Purchased On</Text>
+            <Text className='mt-0'>{dateFormatter.format(order.createdAt)}</Text>
+          </Column>
+          <Column>
+            <Text className='mb-0 text-gray-500 whitespace-nowrap text-nowrap'>Price Paid</Text>
+            <Text className='mt-0'>{formatCurrency(order.pricePaidIncents / 100)}</Text>
+          </Column>
+        </Row>
+      </Section>
+      <Section className='border boreder-solid border-gray-500 rounded-lg p-4 md:p-6 my-4'>
+        <Img src={product.img}/>
+      </Section>
+    </>
   )
 }
 
